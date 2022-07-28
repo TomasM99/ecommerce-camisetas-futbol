57fd8d53ce06aa88a9c0ef9ef4f5ec5756c2691d
Tomas Margio <tomasmr99@gmail.com>
2022-07-28 15:53:15 -0300

    Agrego + productos

--- src/utils/products.js
+++ src/utils/products.js
@@ -41,43 +41,43 @@ const products = [
     },
     {
         id:6,
-        nombre: "Camiseta suplente Argentina 2022",
-        descripcion: "bla bla",
-        precio: 1,
-        stock: 1,
-        imagen: ''
+        nombre: "Camiseta suplente Argentina 2021",
+        descripcion: "Camiseta suplente de la seleccion nacional usada en las eliminatorias y Copa America 2021",
+        precio: 14500,
+        stock: 15,
+        imagen: 'camiseta-argentina-suplente.jpg'
     },
     {
         id:7,
         nombre: "Camiseta suplente Boca 2021",
-        descripcion: "bla bla",
-        precio: 1,
-        stock: 1,
-        imagen: ''
+        descripcion: "Camiseta suplente de Boca usada en el torneo local 2021",
+        precio: 5000,
+        stock: 2,
+        imagen: 'camiseta-boca-suplente.webp'
     },
     {
         id:8,
         nombre: "Camiseta suplente River 2021",
-        descripcion: "bla bla",
-        precio: 1,
-        stock: 1,
-        imagen: ''
+        descripcion: "Camiseta suplente de River usada en el torneo local 2021",
+        precio: 5000,
+        stock: 2,
+        imagen: 'camiseta-river-suplente.webp'
     },
     {
         id:9,
-        nombre: "Camiseta Uruguay",
-        descripcion: "bla bla",
-        precio: 1,
-        stock: 1,
-        imagen: ''
+        nombre: "Camiseta titular Uruguay 2021",
+        descripcion: "Camiseta usada por Uruguay en la Copa America de Brasil 2021",
+        precio: 13000,
+        stock: 2,
+        imagen: 'camiseta-uruguay.webp'
     },
     {
         id:10,
-        nombre: "Camiseta PSG",
-        descripcion: "bla bla",
-        precio: 1,
-        stock: 1,
-        imagen: ''
+        nombre: "Camiseta titular PSG 21/22",
+        descripcion: "Camiseta Nike usada por el equipo parisino en la temporada pasada 21/22",
+        precio: 12000,
+        stock: 8,
+        imagen: 'camiseta-psg.jpg'
     },
 ]
 
