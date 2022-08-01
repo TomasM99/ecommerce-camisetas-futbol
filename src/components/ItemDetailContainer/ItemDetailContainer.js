68afc7876c6f8d2030de678ccc1b3fc2851405cb
Tomas Margio <tomasmr99@gmail.com>
2022-08-01 18:25:42 -0300

    Hago filtrado de item

--- src/components/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -1,17 +1,23 @@
 import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import './ItemDetailContainer.scss';
 import ItemDetail from '../ItemDetail/ItemDetail';
 import products from '../../utils/products';
 
 function ItemDetailContainer({division}) {
 
-    const [item, setItem] = useState([]);
+    const [item, setItem] = useState({});
+    const {id} = useParams();
 
     const getItem = new Promise((resolve, reject) => {
         setTimeout( () => {
-            resolve(products[0])
+            resolve(filterItem())
         }, 500)
     });
+
+    function filterItem(){
+        return products[parseInt(id) - 1];
+    }
 
     useEffect( () => {
         getItem.then((res) => {
