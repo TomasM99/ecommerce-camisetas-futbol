765ca0e014947fc28ed9cabb5c8356a5c40beaf3
Tomas Margio <tomasmr99@gmail.com>
2022-08-17 00:41:47 -0300

    Agrego condicional

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -1,9 +1,18 @@
 import Cart from "../components/Cart/Cart"
+import { useContext } from 'react';
+import { CartContext } from '../context/CartContext';
 
 const Checkout = () => {
+    const {total} = useContext(CartContext);
+
     return(
         <section>
-            <Cart />
+            {total === 0 && (
+                <h2>El carrito esta vacio</h2>
+            )}
+            {total !== 0 && (
+                <Cart />
+            )}
         </section>
     )
 }
