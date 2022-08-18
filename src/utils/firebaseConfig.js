3fd30c88791d92bd1a5030d005adad548fa0f3be
Tomas Margio <tomasmr99@gmail.com>
2022-08-18 16:16:02 -0300

    Agrego dependencias de firebase

---
+++ src/utils/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCAs9tWr1E3QBPXZkp8HcdC-UC6JIHtUTs",
+  authDomain: "ecommerce-camisetas-futbol.firebaseapp.com",
+  projectId: "ecommerce-camisetas-futbol",
+  storageBucket: "ecommerce-camisetas-futbol.appspot.com",
+  messagingSenderId: "245730142005",
+  appId: "1:245730142005:web:2e9a4ec94ced10eb45ce7c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
