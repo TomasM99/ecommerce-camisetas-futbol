d43cd32a8a10c2ed6ffc0b13b810544f14715cc7
Tomas Margio <tomasmr99@gmail.com>
2022-08-29 02:51:18 -0300

    Termino favoritos

--- src/components/Favs/Favs.js
+++ src/components/Favs/Favs.js
@@ -1,9 +1,27 @@
 import './Favs.scss';
+import { useContext } from 'react';
+import { FavContext } from '../../context/FavContext';
+import Item from '../Item/Item';
 
 const Favs = () => {
+    const {productosFav, clear} = useContext(FavContext);
+
+    function avisarConfirmacion(mensaje){
+        console.log(mensaje);
+    }
+
     return(
-        <section>
-            <h1>Favs a desarrollar</h1>
+        <section className='seccion-favs'>
+            <div className='titulo-favs'>
+                <h1>Favoritos</h1>
+                <img src={`/assets/trash.png`} alt="Tachito" className='tachito-fav' onClick={() => {
+                    clear();
+                    avisarConfirmacion("Carrito vaciado!");
+                }}/>
+            </div>
+            <div className='lista-favs'>
+                {productosFav.map((producto) => {return <Item key={producto.id} data={producto} />})}
+            </div>
         </section>
     )
 }
