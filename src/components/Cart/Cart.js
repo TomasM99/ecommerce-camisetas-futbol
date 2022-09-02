0314ef1f6d39f711f7a607ad6309ef189b9b1bff
Tomas Margio <tomasmr99@gmail.com>
2022-09-02 01:18:42 -0300

    Empiezo con estilos formulario de compra

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -7,13 +7,15 @@ import ItemCart from './ItemCart';
 import Modal from '../Modal/Modal';
 import db from '../../utils/firebaseConfig';
 import { collection, addDoc } from 'firebase/firestore'
+import BeatLoader from "react-spinners/BeatLoader";
 
 function Cart() {
     const {productosCart, total, clear} = useContext(CartContext);
     let totalPagar = productosCart.map(prod => (prod.cantidad * prod.precio)).reduce((prev, curr) => prev + curr, 0);
 
     const [showModal, setShowModal] = useState(false);
-    const [success, setSuccess] = useState();
+    const [spinner, setSpinner] = useState(false)
+    const [success, setSuccess] = useState(false);
 
     const [dataForm, setDataForm] = useState({
         name: "",
@@ -63,8 +65,12 @@ function Cart() {
     }
     function handleSubmit(e){
         e.preventDefault();
-        avisarCompra("Compra realizada!")
         storeOrder({...orden, buyer: dataForm});
+        setSpinner(true);
+        setTimeout(() => {
+            setSpinner(false);
+            avisarCompra("Compra realizada!");
+        }, 1500)
     }
     const storeOrder = async (newOrder) => {
         const collectionOrder = collection(db, 'ordenes');
@@ -96,39 +102,85 @@ function Cart() {
                 </div>
             </div>
             {showModal &&
-                <Modal title={"Datos de contacto"} close={() => setShowModal()}>
+                <Modal close={() => setShowModal()}>
                     {success ? (
                         <>
-                            <h2>Orden generada</h2>
+                            <h2 className='titulo-modal'>Compra exitosa!</h2>
                             <p>Orden con ID: {success}</p>
                         </>
                     ) : (
-                        <form onSubmit={handleSubmit}>
-                        <input 
-                            type='text' 
-                            name='name' 
-                            placeholder='Tu nombre'
-                            onChange={handleChange}
-                            value={dataForm.name}
-                        />
-                        <input 
-                            type='number'
-                            name='phone'
-                            placeholder='Tu celular'
-                            onChange={handleChange}
-                            value={dataForm.phone}
-                        />
-                        <input 
-                            type='email'
-                            name='email'
-                            placeholder='Tu mail'
-                            onChange={handleChange}
-                            value={dataForm.email}
-                        />
-                        <button type="submit">Realizar compra</button>
-                    </form>
+                        <>
+                            <h2 className='titulo-modal'>Datos de Contacto</h2>
+                            <form className='formulario-compra' onSubmit={handleSubmit}>
+                                <div className='formulario-datos'>
+                                    <div className='formulario-datos-contacto'>
+                                        <label>Nombre completo</label>
+                                        <input for='nombre'
+                                            id='nombre'
+                                            type='text' 
+                                            name='name' 
+                                            placeholder='Tu nombre'
+                                            onChange={handleChange}
+                                            value={dataForm.name}
+                                        />
+                                        <label for='telefono'>Telefono</label>
+                                        <input 
+                                            id='telefono'
+                                            type='number'
+                                            name='phone'
+                                            placeholder='Tu celular'
+                                            onChange={handleChange}
+                                            value={dataForm.phone}
+                                        />
+                                        <label for='mail'>Mail de contacto</label>
+                                        <input 
+                                            id='mail'
+                                            type='email'
+                                            name='email'
+                                            placeholder='Tu mail'
+                                            onChange={handleChange}
+                                            value={dataForm.email}
+                                        />
+                                    </div>
+                                    <div className='formulario-datos-pago'>
+                                        <div className='formulario-radio'>
+                                            <h4>Metodo de pago</h4>
+                                            <div>
+                                                <input id='efectivo' type='radio' name='pago'></input>
+                                                <label for='efectivo'>Efectivo</label>
+                                            </div>
+                                            <div>
+                                                <input id='mp' type='radio' name='pago'></input>
+                                                <label for='mp'>Mercado Pago</label>
+                                            </div>
+                                            <div>
+                                                <input id='tarjeta' type='radio' name='pago'></input>
+                                                <label for='tarjeta'>Tarjeta</label>
+                                            </div>
+                                        </div>
+                                        <div className='formulario-entrega'>
+                                            <div><strong>ENVIO GRATIS</strong></div>
+                                            <img src={`/assets/entrega.png`} alt="Envio"/>
+                                        </div>
+                                        <div className='formulario-entrega'>
+                                            <div><strong>RETIRO EN TIENDA</strong></div>
+                                            <img src={`/assets/tienda.png`} alt="Tienda"/>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className='formulario-final'>
+                                    <img src={`/assets/diamante-compra.png`} alt="Diamante compra"/>
+                                    <button className='btn btn-outline-dark' type="submit">Realizar compra</button>
+                                </div>
+                            </form>
+                        </>
                     )}
                 </Modal>
+            }
+            {spinner &&
+                <div className='spinner-cart'>
+                    <BeatLoader color="#000000" loading margin={5} size={10} />
+                </div>
             }
         </div>
     );
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,10 +1,19 @@
 import './Modal.scss';
+import { useContext } from 'react';
+import { CartContext } from '../../context/CartContext';
+import {Link} from 'react-router-dom';
 
-const Modal = ({title, close, children}) => {
+const Modal = ({close, children}) => {
+    const {clear} = useContext(CartContext);
+
     return(
         <div className='modal-container'>
-            <h2>{title}</h2>
-            <img src={`/assets/cancel.png`} alt="Tachito" onClick={() => close(false)}/>
+            <Link to="/">
+                <img src={`/assets/cancel.png`} alt="Tachito" className='cruz' onClick={() => {
+                    close(false);
+                    clear();
+                }}/>
+            </Link>
             {children}
         </div>
     )
