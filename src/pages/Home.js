9fd09688afca8013d682b62bd8812da24c2c7155
Tomas Margio <tomasmr99@gmail.com>
2022-09-09 01:41:07 -0300

    Hago banner home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,24 @@
+import './Home.scss';
+
 const Home = () => {
     return(
-        <h1>Home a desarrollar</h1>
+        <>
+            <div className='home-banner'>
+                <div className='banner-texto'>
+                    <div>
+                        <h1>LA PASIÓN POR LA SELECCIÓN</h1>
+                        <p>La camiseta suplente pronto estará disponible para la compra. Envío gratis a todo el país!</p>
+                    </div>
+                    <div>
+                        <button className='btn btn-outline-dark banner-boton'>RESERVAR AHORA</button>
+                    </div>
+                </div>
+                <div className='banner-imagenes'>
+                    <img src='/assets/banner-1.webp' alt='Banner' className='banner-i-1'/>
+                    <img src='/assets/banner-2.webp' alt='Banner' className='banner-i-2'/>
+                </div>
+            </div>
+        </>
     )
 }
 
