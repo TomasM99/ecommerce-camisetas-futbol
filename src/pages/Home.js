23e9d690e21efee58aac8ff1c8cf654cf36863cb
Tomas Margio <tomasmr99@gmail.com>
2022-09-09 02:02:44 -0300

    Termino home, ponele

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,32 @@
 import './Home.scss';
+import {Link} from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import ItemList from '../components/ItemList/ItemList';
+import { collection, getDocs} from "firebase/firestore"
+import db from '../utils/firebaseConfig';
 
 const Home = () => {
+    const [listaProductos, setListaProductos] = useState([]);
+
+    const getProductos = async() => {
+        const productCollection = collection(db, 'productos');
+        const productSnapshot = await getDocs(productCollection);
+        const productList = productSnapshot.docs.map( (doc) => {
+            let product = doc.data();
+            product.id = doc.id;
+            return product;
+        });
+        return productList.splice(0, 4);
+    }
+
+    useEffect( () => {
+        getProductos().then((res) => {
+            setListaProductos(res);
+        }).catch((error) => {
+            console.log("No se pudieron cargar los productos");
+        })
+    }, [])
+
     return(
         <>
             <div className='home-banner'>
@@ -16,6 +42,17 @@ const Home = () => {
                 <div className='banner-imagenes'>
                     <img src='/assets/banner-1.webp' alt='Banner' className='banner-i-1'/>
                     <img src='/assets/banner-2.webp' alt='Banner' className='banner-i-2'/>
+                </div>
+            </div>
+            <div className='home-botones-categorias'>
+                <Link to="/category/seleccion" className='boton-link'><button className='btn btn-outline-dark boton-categoria'>SELECCIONES</button></Link>
+                <Link to="/category/club-argentino" className='boton-link'><button className='btn btn-outline-dark boton-categoria'>CLUBES ARGENTINOS</button></Link>
+                <Link to="/category/club-europeo" className='boton-link'><button className='btn btn-outline-dark boton-categoria'>CLUBES EUROPEOS</button></Link>
+            </div>
+            <div>
+                <h1 className='home-titulo'>Productos populares</h1>
+                <div className='home-lista-items'>
+                    <ItemList data={listaProductos}/>
                 </div>
             </div>
         </>
